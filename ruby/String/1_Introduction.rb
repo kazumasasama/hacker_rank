10ede1532b5a970eb8e0bbccd406fcbd1ccf1d4f
Kaz <takaik@yahoo.com>
2022-04-17 15:31:15 -0400

    String/1_Introduction

---
+++ ruby/String/1_Introduction.rb
@@ -0,0 +1,15 @@
+def single_quote
+  # single quote string here
+  return 'Hello World and others!'
+end
+
+def double_quote
+  # Double quote string here
+  return "Hello World and others!"
+end
+
+def here_doc
+  # Here doc string here
+  document = "Hello World and others!"
+  return document
+end
