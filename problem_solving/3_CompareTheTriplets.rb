de74b4d6bc9a13ca3300ff7310ede21fe5a824af
Kaz <takaik@yahoo.com>
2022-05-29 10:12:17 -0400

    add description - problem_solving/ 3_CompareTheTriplets

--- problem_solving/3_CompareTheTriplets.rb
+++ problem_solving/3_CompareTheTriplets.rb
@@ -1,17 +1,31 @@
+a = [5, 6, 7]
+b = [3, 6, 10]
+
 def compareTriplets(a, b)
   alice = 0
   bob = 0
-  result = []
   i = 0
   while i < a.length
-      if a[i] < b[i]
-        bob += 1
-      elsif a[i] > b[i]
-        alice += 1
-      end
-      i += 1
+    if a[i] < b[i]
+      bob += 1
+    elsif a[i] > b[i]
+      alice += 1
+    end
+    i += 1
   end
-  result << alice
-  result << bob
-  return result
+  return [alice, bob]
 end
+
+p compareTriplets(a, b)
+
+# ===================
+
+# アリスとボブの得点を比較して得点が大きい方にポイントを付与し、それぞれのポイント数を配列で返す。
+# 得点が同じだった場合はどちらにもポイントは付かない。
+# whileループの中で配列aと配列bの同じインデックスの値を比較し、値の大きかった人にポイントを付与
+
+# ===================
+
+# In the while loop, compare each values of same index of a and b arrays.
+# Give 1 point to the person if it's score is bigger than the other person.
+# When both of them has same score, do nothing.
