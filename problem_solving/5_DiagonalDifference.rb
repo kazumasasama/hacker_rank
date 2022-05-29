150503d5b1a4f223964d06c7f7b0190eb36c0147
Kaz <takaik@yahoo.com>
2022-05-29 11:02:58 -0400

    add description - problem_solving/ 5_DiagonalDifference

--- problem_solving/5_DiagonalDifference.rb
+++ problem_solving/5_DiagonalDifference.rb
@@ -1,3 +1,5 @@
+arr = [[11, 2, 4], [4, 5, 6], [10, 8, -12]]
+
 def diagonalDifference(arr)
   i = 0
   j = 0
@@ -20,3 +22,18 @@ def diagonalDifference(arr)
   end
   return ltor - rtol
 end
+
+# ====================
+
+# n * n マトリックスの左右斜めの合計の差を求める問題。
+# （左上から右下）- (右上から左下) = 答え
+
+# 配列の中に配列が入っているのでwhileループの中でインデックスを2つ使いそれぞれの必要な値を取得して合計値を取得。
+
+# 右からの場合は中の配列のインデックスを後ろから取得している。
+
+# ====================
+
+# use two indexes ('i' and 'j') in the while loops to get specific values of arrays in arr
+
+# set 'j' as index of end of array, decrease it in the loop to get right to left diagnonal sum.
