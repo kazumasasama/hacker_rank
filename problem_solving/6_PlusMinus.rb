412a225a2918c32e20d3f5ff1741cb5880ab0dd4
Kaz <takaik@yahoo.com>
2022-05-29 11:43:08 -0400

    add description - problem_solving/ 6_PlusMinus

--- problem_solving/6_PlusMinus.rb
+++ problem_solving/6_PlusMinus.rb
@@ -1,3 +1,5 @@
+arr = [-4, 3, -9, 0, 4, 1]
+
 def plusMinus(arr)
   pos = 0
   neg = 0
@@ -15,3 +17,19 @@ def plusMinus(arr)
   puts sprintf("%.6f", neg/arr.length.to_f)
   puts sprintf("%.6f", zero/arr.length.to_f)
 end
+
+plusMinus(arr)
+
+# ====================
+
+arr配列中の値の正、負、ゼロ、それぞれの割合を求める問題。
+
+eachループでarr内のそれぞれの値の正、負、ゼロそれぞれの数を取得。
+sprintfの第一引数には返値の桁数を指定し、第二引数に正、負、ゼロの割合を渡す。
+
+# ====================
+
+count and store each numbers of positives, negatives and zeros to each valiables.
+
+the first argument of sprintf() is number of digits afater the decimal.
+the second argument is decimal number that is ratio.
