932a4462796df7512527f708e3c0b4d4bb442b31
kt4 <kaz.nyc.dev@gmail.com>
2022-08-13 16:23:01 -0400

    add description/ 58_Encryption.rb

--- problem_solving2/58_Encryption.rb
+++ problem_solving2/58_Encryption.rb
@@ -1,12 +1,14 @@
 def encryption(s)
   floor = Math.sqrt(s.length).floor
   ceil = Math.sqrt(s.length).ceil
+  if floor * ceil < s.length
+    floor += 1
+  end
   loop_times = 0
   string = s
   while string.length % floor != 0
     string += "*"
   end
-  p string
   devided = string.split(//)
   i = 0
   j = ceil
@@ -22,36 +24,60 @@ def encryption(s)
   return indices.map{|index| devided[index]}.join().scan(/#{"." * floor}/).join(" ").delete("*")
 end
 
-s = "chillout"
+
+s = "haveaniceday"
 p encryption(s)
 
 # ====================
 # 問題:
-#   i < j < k
-#   a[j] - a[i] = a[k] - a[j] = d
-#   以上の条件に合致する[i, j, k]の組み合わせがいくつあるかを返す。
+#   与えられた英文を暗号化する。
+#   暗号化の手順は以下。
+
+#   1.  英文からはスペースが取り除かれている
+
+#   2.  その英文の文字数の平方根(何の2乗なのか)を求める。
+#       整数でない平方根が得られる場合、四捨五入した数xと繰り上げた数yを求める。
+#       整数の場合は x = y とする。x * y > s.length の条件を満たさない場合は x + 1。
+
+#   3.  xを行、yを一行の最大文字数とする。最後の行は余りの文字なので文字数はyでなくても良い。
+
+#   4.  生成されたものを縦読みし、一行縦読みする毎にスペースを入れる。
+
+#   5.  完了!
+
 
 # 条件:
-#   nは引数に渡されていませんが使用しなくても解答を得ることができるので無視してください。
+#   引数で渡されてくる文字列にはスペースが入っていると説明文に記載がありますが、実際には入っていません。
 
-#   n ->    arr配列の長さ
-#   d ->    マッチする値
-#   arr ->  小さい順にソートされた数値の配列
+#   s -> 小文字のアルファベットのみの文字列。記号やその他の文字は入っていない。
 
 # ポイント解説:
-#   Line 5: if arr.include?(arr[i] + d) && arr.include?(arr[i] + d * 2)
-#     arr.include?(arr[i] + d)
-#       -> (a[j] - a[i] = d) # の条件に合致する値があればtrue
-#     arr.include?(arr[i] + d * 2)
-#       -> (a[k] - a[j] = d) # の条件に合致する値があればtrue
+#   今回は文字列を一文字ずつ分解し、どのように組み替えるのかに焦点を当てました。
+#   インデックスを配列に生成し、そのインデックスを元に文字を組み替えました。
 
-#     # 両方ともtrueだった場合countに1足す
+#   Line 2:
+#     floor = Math.sqrt(s.length).floor
+#       平方根を求める方法はいくつかありますが、rubyにはビルトインメソッドがあるのでそれを利用しました。
+#       Math.sqrt()はdecimalが返ってくるのでfloorやceilを使って整数にしています。
 
-#     # arr[k]は (arr[i] + d * 2) で求めることができる。
+#   Line 24:
+#     return indices.map{|index| devided[index]}.join().scan(/#{"." * floor}/).join(" ").delete("*")
+#       記号は使われないという条件を利用し、.scan()で均等に分配するためにLine10で"*"を必要分追加しています。
+#       この行で"*"を排除しているのはそのためです。
 
-#   Line 12:
-#   return arr.select{|num| arr.include?(num + d) && arr.include?(num + (d * 2))}.length
-#     一行で記述も可能です。
-#     .selectで条件にマッチした値を抽出した配列を作成しその値の数を返す。
-
+#   少しブルートフォースなコードになっているかもしれません。
+#   より良い書き方がありましたらぜひ教えていただけると嬉しいです！
 # ====================
+
+# Line 2:
+# floor = Math.sqrt(s.length).floor
+#   Use Math.sqrt() to get square root.
+#   When square root is not an integer, this builtin method returns decimal.
+
+# Line 24:
+# return indices.map{|index| devided[index]}.join().scan(/#{"." * floor}/).join(" ").delete("*")
+#   The string s is only contains lowercase alphabets so you can use "*" to adjust the length to use .scan() to distribute the reordered string.
+
+# This solution might be brute force.
+# Let me know if you have better solution.
+# I always love to learn!
